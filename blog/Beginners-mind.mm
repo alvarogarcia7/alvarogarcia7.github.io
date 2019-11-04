a428366766ad8a4bfd9ac013801d839b12cdefbb
alvarogarcia7 (via Travis CI) <alvarogarcia7@users.noreply.github.com>
2019-11-04 09:59:35 +0000

    rebuilding site Mon Nov  4 09:59:34 UTC 2019

---
+++ blog/Beginners-mind.mm
@@ -0,0 +1,182 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1523105088880" ID="ID_1489559476" MODIFIED="1523105095395" TEXT="Beginner&apos;s mind">
+<node CREATED="1523105096047" ID="ID_142925938" MODIFIED="1523105103078" POSITION="right" TEXT="In things you don&apos;t master">
+<node CREATED="1523105103714" ID="ID_1432383491" MODIFIED="1523105149779" TEXT="Otherwise you&apos;re in your confort zone"/>
+<node CREATED="1523105367694" ID="ID_679135277" MODIFIED="1523105379399" TEXT="At the limit of your knowledge"/>
+</node>
+<node CREATED="1523105156881" ID="ID_1470608481" MODIFIED="1523105299518" POSITION="left" TEXT="Refreshing to experience it">
+<node CREATED="1523105347338" ID="ID_1154578520" MODIFIED="1523105365742" TEXT="Feeling like a pioneer"/>
+<node CREATED="1523105387416" ID="ID_16831520" MODIFIED="1523105395111" TEXT="Many patterns to learn"/>
+<node CREATED="1523105704488" ID="ID_480227023" MODIFIED="1523105715149" TEXT="Feeling a complete beginner again"/>
+<node CREATED="1523105715966" ID="ID_872310004" MODIFIED="1523105757005">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Moments of your childhood,
+    </p>
+    <p>
+      where you discovered and
+    </p>
+    <p>
+      learned many topics/areas
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1523105768463" ID="ID_675065991" MODIFIED="1523105793270">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      But this effect soon
+    </p>
+    <p>
+      disappears if you
+    </p>
+    <p>
+      practice often enough
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1523105794244" ID="ID_1895587430" MODIFIED="1523105811622">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      So you must keep it low&#160;
+    </p>
+    <p>
+      to keep reaping the effects
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1523105167865" ID="ID_143013492" MODIFIED="1523105174759" POSITION="right" TEXT="Cure of humility">
+<node COLOR="#996600" CREATED="1523105174760" ID="ID_1972155468" MODIFIED="1523105258114" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Necessary when you're put in a position of authority
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1523105636417" ID="ID_1323178296" MODIFIED="1523105636417" TEXT=""/>
+</node>
+<node CREATED="1523105397774" ID="ID_273210126" MODIFIED="1523105432975" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Can be experienced
+    </p>
+    <p>
+      &#160;in many areas
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1523105554919" ID="ID_1508777024" MODIFIED="1523105573458" TEXT="Examples">
+<node CREATED="1523105454005" ID="ID_1242548519" MODIFIED="1523105462858" TEXT="People">
+<node CREATED="1523105465431" ID="ID_1538462904" MODIFIED="1523105468281" TEXT="Individuals"/>
+<node CREATED="1523105468990" ID="ID_1067230265" MODIFIED="1523105482728" TEXT="People relationshions"/>
+</node>
+<node CREATED="1523105511505" ID="ID_1569874676" MODIFIED="1523105513146" TEXT="Work">
+<node CREATED="1523105436557" ID="ID_454872102" MODIFIED="1523105444314" TEXT="Physical work"/>
+<node CREATED="1523105446701" ID="ID_678319017" MODIFIED="1523105453376" TEXT="Knowledge work"/>
+</node>
+<node CREATED="1523105520498" ID="ID_1620584540" MODIFIED="1523105524834" TEXT="Fun"/>
+<node CREATED="1523105528955" ID="ID_213328706" MODIFIED="1523105531643" TEXT="Sports"/>
+<node CREATED="1523105537563" ID="ID_1334407326" MODIFIED="1523105541852" TEXT="Volunteering"/>
+</node>
+<node CREATED="1523105575374" ID="ID_1848357510" MODIFIED="1523105594993">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The effect does not
+    </p>
+    <p>
+      depend on the area
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1523105612334" ID="ID_398513046" MODIFIED="1523105628738">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Good to mix areas
+    </p>
+    <p>
+      for maximum effect
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1523105649596" ID="ID_372825587" MODIFIED="1523105668914" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Yet another opportunity
+    </p>
+    <p>
+      to have skin in the game
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1523105672302" ID="ID_506764813" MODIFIED="1523105702092">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      You're new in this area,
+    </p>
+    <p>
+      therefore no one
+    </p>
+    <p>
+      (neither you or anyone else)
+    </p>
+    <p>
+      expects you to be proficient
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
